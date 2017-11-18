11a7696f3cbd7929f3823f831ab62a895f3a1ccc
TRiBByX <christoffer.ssj@gmail.com>
2017-11-18 08:15:02 +0100

    Added vmhandler.py

--- Load_Balancer/handlers/vmhandler.py
+++ Load_Balancer/handlers/vmhandler.py
@@ -1,4 +1,4 @@
-import requests
+# import requests
 from oauth2client.client import GoogleCredentials
 from googleapiclient.discovery import build
 
@@ -7,13 +7,15 @@ def start_vm():
     credentials = GoogleCredentials.get_application_default()
     compute = build('compute', 'v1', credentials=credentials)
 
-    #r = compute.instances().reset
+    # r = compute.instances().reset
 
-    print(compute.instances().start(project='upbeat-medley-184111', zone='us-central1-f', instance='instance-1').execute())
-    #print(compute.instances().list(project='upbeat-medley-184111', zone='us-central1-f').execute())
-    
-    #r = requests.post('https://www.googleapis.com/compute/v1/projects/upbeat-medley-184111/zones/us-central1-f/instances/instance-1/stop?access_token={0}'.format(compute))
+    print(compute.instances().start(project='upbeat-medley-184111',
+                                    zone='us-central1-f',
+                                    instance='instance-1').execute())
 
+    # print(compute.instances().list(project='upbeat-medley-184111',
+    #                                zone='us-central1-f').execute())
+    # r = requests.post('https://www.googleapis.com/compute/v1/projects/upbeat-medley-184111/zones/us-central1-f/instances/instance-1/stop?access_token={0}'.format(compute))
 
     pass
 
