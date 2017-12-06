64ad7297961ab11e0ac6ea5a1ae65d51033261a1
TRiBByX <christoffer.ssj@gmail.com>
2017-12-06 14:12:54 -0800

    Created a tcp loader in the django code, just need to test it

--- Django/AdminPortal/panel/urls.py
+++ Django/AdminPortal/panel/urls.py
@@ -1,6 +1,5 @@
 from django.conf.urls import url
 from panel.views import Index
-from django.views.generic.base import RedirectView
 
 urlpatterns = [
     url(r'^$', Index.as_view(), name='dashboard')
--- Django/AdminPortal/panel/views.py
+++ Django/AdminPortal/panel/views.py
@@ -3,11 +3,22 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.views.generic import View
 
+import socket
+import ast
+
 
 class Index(View):
     """Index class, just the fronpage of the site."""
 
     def get(self, request):
         """Get method for the index."""
-        
-        return(render(request, 'panel/dashboard.html'))
+        s = socket.socket()
+        host = socket.gethostname()
+        port = 8888
+
+        s.connect((host, port))
+
+        s.send('get')
+
+        context = {'servers': ast.literal_eval(s.recv(1024))}
+        return(render(request, 'panel/dashboard.html', context=context))
